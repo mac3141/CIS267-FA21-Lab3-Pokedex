a2a276765db018ef636a9f1e68b4e2c1f46f6bf2
Madison Cagle <78162094+mac3141@users.noreply.github.com>
2021-10-06 13:19:53 -0500

    Add getPokemon()

--- script.js
+++ script.js
@@ -3,28 +3,36 @@ const pokemon_count = 150;
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
-	electric: '#FCF7DE',
-	water: '#DEF3FD',
-	ground: '#f4e7da',
-	rock: '#d5d5d4',
-	fairy: '#fceaff',
-	poison: '#98d7a5',
-	bug: '#f8d5a3',
-	dragon: '#97b3e6',
-	psychic: '#eaeda1',
-	flying: '#F5F5F5',
-	fighting: '#E6E0D4',
-	normal: '#F5F5F5'
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5'
 };
 
-const main_types = Object.keys(colors);
+const main_types = Object.keys(colors); // ["fire", "grass", "electric", ...]
 
-const fetchPokemons = async () => {
-    for(let i = 1; i <= pokemon_count; i++) {
+const fetchPokemon = async () => {
+    for (let i = 1; i <= pokemon_count; i++) {
         await getPokemon(i);
     }
 };
 
+const getPokemon = async function (id) {
+    // Get Pokemon data from pokeapi
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+
+    createPokemonCard(data);
+};
 
 const createPokemonCard = (pokemon) => {
     const pokemonEl = document.createElement('div');
@@ -58,4 +66,4 @@ const createPokemonCard = (pokemon) => {
     poke_container.appendChild(pokemonEl);
 };
 
-fetchPokemons();
+fetchPokemon();
