794b9887a15c021bd5044f3590782e2d8fb401f4
Madison Cagle <78162094+mac3141@users.noreply.github.com>
2021-10-16 20:36:01 -0500

    Fix favorites to work by filter, very tiny visual improvements

--- script.js
+++ script.js
@@ -24,24 +24,22 @@ const typeColors = {
 const main_types = Object.keys(typeColors); // ["fire", "grass", "electric", ...]
 
 let allPokemon = [];
-let favorites = [];
 let pokemonSpecies = [];
 
 const fetchPokemon = async () => {
     for (let i = 1; i <= pokemon_count; i++) {
-        let p = await getPokemon(i);
-        p.isFavorite = false;
-        p.isLengendary = false; // working on it
-        allPokemon.push(p);
+        let pokemon = await getPokemon(i);
+        let species = await getPokemonSpecies(i);
+        pokemon.isFavorite = false;
+        pokemon.isLengendary = species.habitat.is_legendary;
+        allPokemon.push(pokemon);
     }
 };
 
 const fetchPokemonSpecies = async () => {
     for (let i = 1; i <= pokemon_count; i++) {
         let p = await getPokemonSpecies(i);
-        if (p.habitat.is_legendary) {
-            pokemonSpecies.push(p);
-        }
+        pokemonSpecies.push(p);
     }
 };
 
@@ -108,7 +106,6 @@ const createPokemonCard = (pokemon) => {
 
     poke_container.appendChild(pokemonEl);
 
-    // Now I need to get the heart to change color again when I hover over it. Otherwise it works.
     const heart = document.getElementById(pokemon.id);
     heart.style.color = pokemon.isFavorite ? "#F52F07" : "rgba(255, 255, 255, 0.5)";
 
@@ -122,25 +119,13 @@ const createPokemonCard = (pokemon) => {
         }
     });
 
-    heart.addEventListener("click", () => {
-        updateFavorites(pokemon);
+    heart.addEventListener("click", event => {
+        event.preventDefault();
+        pokemon.isFavorite = !pokemon.isFavorite;
         heart.style.color = pokemon.isFavorite ? "#F52F07" : "rgba(255, 255, 255, 0.5)";
         // console.log(favorites);
     });
 };
-
-function updateFavorites(pokemon) {
-    pokemon.isFavorite = !pokemon.isFavorite;
-
-    // push if isFavorite is true
-    if (pokemon.isFavorite) {
-        favorites.push(pokemon);
-    }
-    // remove if isFavorite is false
-    else {
-        favorites.splice(favorites.indexOf(pokemon), 1);
-    }
-}
 
 async function loadAllPokemon() {
     await fetchPokemon();
@@ -158,15 +143,11 @@ function updateSearchResults() {
     console.log(searchQuery);
     console.log(typeof (searchQuery));
 
-
-
     // search by name or id, HALLELUJAH FIRST TRY
     let searchResults = allPokemon.filter(pokemon => {
         if (pokemon.name.toLowerCase().includes(searchQuery.toLowerCase()) || pokemon.id.toString().includes(searchQuery)) {
             return true;
         }
-
-        //return pokemon.name.toLowerCase().includes(searchQuery.toLowerCase());
     });
 
     clearPokemon();
@@ -186,13 +167,19 @@ searchButton.addEventListener("click", () => {
 all.addEventListener("click", () => {
     clearPokemon();
     renderPokemon(allPokemon);
+
     all.classList.add("active");
     favs.classList.remove("active");
 });
 
 favs.addEventListener("click", () => {
+    let favorites = allPokemon.filter(pokemon => {
+        return pokemon.isFavorite;
+    });
+
     clearPokemon();
     renderPokemon(favorites);
+
     favs.classList.add("active");
     all.classList.remove("active");
 });
